3174a96b791407bc4871ebd65af9d41f4cdc9b6c
José Henn <joseh.rang@gmail.com>
2026-02-06 16:15:26 -0300

    31

---
+++ src/main/java/br/com/maratonajsf/bean/comunicacao/ComunicacaoTeste9Bean.java
@@ -0,0 +1,37 @@
+package br.com.maratonajsf.bean.comunicacao;
+
+import org.omnifaces.util.Faces;
+
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.http.Cookie;
+import java.io.Serializable;
+import java.util.Map;
+
+@Named
+@ViewScoped
+public class ComunicacaoTeste9Bean implements Serializable {
+    private String key;
+    private String value;
+
+    public void init() {
+        value = Faces.getRequestCookie("nome");
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
