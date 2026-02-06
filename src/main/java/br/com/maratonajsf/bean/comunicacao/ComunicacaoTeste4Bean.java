86a437ece74654c715fb45ca89cce34caa9b4389
José Henn <joseh.rang@gmail.com>
2026-02-06 14:04:59 -0300

    28

---
+++ src/main/java/br/com/maratonajsf/bean/comunicacao/ComunicacaoTeste4Bean.java
@@ -0,0 +1,40 @@
+package br.com.maratonajsf.bean.comunicacao;
+
+import br.com.maratonajsf.model.Estudante;
+
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class ComunicacaoTeste4Bean implements Serializable {
+    private String buttonName = "Nome do botão do Bean";
+    private Estudante estudante = new Estudante();
+
+    public void execute(ActionEvent event){
+        String nome = (String) event.getComponent().getAttributes().get("nome");
+        String adjetivo = (String) event.getComponent().getAttributes().get("adjetivo");
+        Estudante estudante = (Estudante) event.getComponent().getAttributes().get("estudante");
+        System.out.println(nome);
+        System.out.println(adjetivo);
+        System.out.println(estudante.getNome());
+    }
+
+    public String getButtonName() {
+        return buttonName;
+    }
+
+    public void setButtonName(String buttonName) {
+        this.buttonName = buttonName;
+    }
+
+    public Estudante getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(Estudante estudante) {
+        this.estudante = estudante;
+    }
+}
